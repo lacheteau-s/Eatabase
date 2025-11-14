540d0e466228cfa4d66f4d7332cb6b814c9c195f
Sullivan Lacheteau <sullivan.lacheteau@outlook.com>
2025-11-14 15:30:44 -0500

    Fetch from database

--- src/Eatabase.API/Features/Products/GetProductDetails.RequestHandler.cs
+++ src/Eatabase.API/Features/Products/GetProductDetails.RequestHandler.cs
@@ -1,9 +1,30 @@
+using Eatabase.API.Data;
+
 namespace Eatabase.API.Features.Products;
 
-internal sealed class GetProductDetailsRequestHandler
+internal sealed class GetProductDetailsRequestHandler(AppDbContext dbContext)
 {
-	public ProductDetails? Handle(Guid id)
+	public async Task<ProductDetails?> HandleAsync(Guid id, CancellationToken ct)
 	{
-		return null;
+		var product = await dbContext.Products.FindAsync([id], ct);
+
+		if (product is null)
+			return null;
+
+		return new ProductDetails(
+			product.Id,
+			product.CreatedAt,
+			product.UpdatedAt,
+			product.Brand,
+			product.Name,
+			product.Calories,
+			product.TotalFat,
+			product.SaturatedFat,
+			product.TransFat,
+			product.TotalCarbs,
+			product.Sugars,
+			product.Fiber,
+			product.Protein
+		);
 	}
 }
